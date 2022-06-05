1087b2c23b741b16bcf5951983879eea68de8025
marcel-ujcic <68743060+marcel-ujcic@users.noreply.github.com>
2022-06-05 20:52:37 +0200

    Add files via upload

---
+++ Debug/Core/Src/fatfs_storage.d
@@ -0,0 +1,174 @@
+Core/Src/fatfs_storage.o: ../Core/Src/fatfs_storage.c \
+ ../Core/Inc/fatfs_storage.h \
+ C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\ff.h \
+ C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\integer.h \
+ ../Core/Inc/ffconf.h ../Core/Inc/main.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ ../Core/Inc/stm32f7xx_hal_conf.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h \
+ ../Drivers/CMSIS/Include/core_cm7.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma2d.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_sdram.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_ll_fmc.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_ltdc.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_ltdc_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dsi.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_qspi.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_sd.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_ll_sdmmc.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim_ex.h \
+ ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dsi.h \
+ ../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery.h \
+ ../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_lcd.h \
+ ../Drivers/BSP/STM32F769I-Discovery/../Components/otm8009a/otm8009a.h \
+ ../Drivers/BSP/STM32F769I-Discovery/../Components/adv7533/adv7533.h \
+ ../Drivers/BSP/STM32F769I-Discovery/../Components/adv7533/../Common/audio.h \
+ ../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_sdram.h \
+ ../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery.h \
+ ../Drivers/BSP/STM32F769I-Discovery/../../../Utilities/Fonts/fonts.h \
+ ../Core/Inc/fatfs_storage.h \
+ C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\ff_gen_drv.h \
+ C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\diskio.h \
+ C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\ff.h \
+ ../Core/Inc/sd_diskio.h \
+ ../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_sd.h \
+ ../Core/Inc/ffconf.h
+
+../Core/Inc/fatfs_storage.h:
+
+C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\ff.h:
+
+C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\integer.h:
+
+../Core/Inc/ffconf.h:
+
+../Core/Inc/main.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+
+../Core/Inc/stm32f7xx_hal_conf.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h:
+
+../Drivers/CMSIS/Include/core_cm7.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv7.h:
+
+../Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma2d.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_sdram.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_ll_fmc.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_ltdc.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_ltdc_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dsi.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_qspi.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_sd.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_ll_sdmmc.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim_ex.h:
+
+../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dsi.h:
+
+../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery.h:
+
+../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_lcd.h:
+
+../Drivers/BSP/STM32F769I-Discovery/../Components/otm8009a/otm8009a.h:
+
+../Drivers/BSP/STM32F769I-Discovery/../Components/adv7533/adv7533.h:
+
+../Drivers/BSP/STM32F769I-Discovery/../Components/adv7533/../Common/audio.h:
+
+../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_sdram.h:
+
+../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery.h:
+
+../Drivers/BSP/STM32F769I-Discovery/../../../Utilities/Fonts/fonts.h:
+
+../Core/Inc/fatfs_storage.h:
+
+C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\ff_gen_drv.h:
+
+C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\diskio.h:
+
+C:\Users\marcel\Documents\3_letnik\VGRS\lcd_rtos\lcd_rtos\Middlewares\Third_Party\src\ff.h:
+
+../Core/Inc/sd_diskio.h:
+
+../Drivers/BSP/STM32F769I-Discovery/stm32f769i_discovery_sd.h:
+
+../Core/Inc/ffconf.h:
